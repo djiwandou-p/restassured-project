f52c1028d1e413dcfee2b299d84fe4b0bad45a92
djiwandou-p <djiwandou@evermos.com>
2021-04-18 08:44:32 +0700

    init mvn project, set dependencies, add simple get request

---
+++ src/main/java/evm/restassured/App.java
@@ -0,0 +1,22 @@
+package evm.restassured;
+import static io.restassured.RestAssured.*;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println(response.statusCode());
+        
+    }
+}
