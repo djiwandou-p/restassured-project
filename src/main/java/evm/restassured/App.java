c530af4a1b0e698916169a8e6cbd253c86bdbaad
djiwandou-p <djiwandou@evermos.com>
2021-04-18 09:26:00 +0700

    capture more response status

--- src/main/java/evm/restassured/App.java
+++ src/main/java/evm/restassured/App.java
@@ -17,6 +17,9 @@ public class App
         System.out.println( "Hello World!" );
         Response response = RestAssured.get("https://reqres.in/api/users?page=2");
         System.out.println(response.statusCode());
+        System.out.println(response.asString());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.statusLine());
         
     }
 }
